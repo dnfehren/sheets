270c31c36e837cf33ce83b928f6fa214c31990ed
Daniel Fehrenbach <dnfehrenbach@gmail.com>
2012-03-29 23:32:57 -0500

    header handling, named tuples for rows

--- sheet_test.py
+++ sheet_test.py
@@ -1,5 +1,21 @@
 import sheetrd
 
-sheet = sheetrd.SheetReader('testing.xlsx')
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
 
-print sheet.sheets[0]['data'][4]
+
+csv_wb = sheetrd.SheetReader('test_data/csv_testing.csv')
+xls_wb = sheetrd.SheetReader('test_data/xls_testing.xls', 1)
+xlsx_wb = sheetrd.SheetReader('test_data/xlsx_testing.xlsx', 1)
+
+#pp.pprint(xls_wb.sheets)
+
+for row in csv_wb.sheet_rows('csv_testing'):
+	print row
+
+#for row in xls_wb.sheet_rows('testing'):
+#	print row
+
+#for name in xlsx_wb.book_names():
+#	for row in xlsx_wb.sheet_rows(name):
+#		print row
--- sheetrd.py
+++ sheetrd.py
@@ -1,22 +1,48 @@
 import os
 import csv, xlrd, openpyxl
+import collections
+
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
+
+'''
+why use tuple for rows?
+http://stackoverflow.com/questions/626759/whats-the-difference-between-list-and-tuples-in-python
+
+row are heterogeneous, right? but a row, when its with other rows is homogeneous
+so... 
+rows are tuples (sometimes named tuples)
+worksheets are named tuples containing a name and data (data is a list of tuples)
+workbooks are lists containing worksheets
+'''
 
 class SheetReader(object):
 
-    def __init__(self, file_location):
+    def __init__(self, file_location, header_row=0):
 
         try:
             file_handle = open(file_location, 'rb')
         except IOError:
             print "can't open file"
 
-        file_path_and_name, file_ext = os.path.splitext(file_location)
+
+        file_basename = os.path.basename(file_location)
+        file_name, file_ext = os.path.splitext(file_basename)
+
+        '''
+        using a named tuple for worksheets to contain the sheet's 
+        name and data in a simple object
+        http://docs.python.org/library/collections.html#collections.namedtuple
+        '''
+        Worksheet = collections.namedtuple('Worksheet', 
+                                           ['sheet_name','sheet_data'])
 
         if file_ext == '.csv':
 
             csv_reader = csv.reader(file_handle)
 
             '''
+            TODO
             #what about really big sheets?
             file_size = os.path.getsize(file_location)
 
@@ -24,15 +50,59 @@ class SheetReader(object):
                 #then what? can't access csv by row/col indexs
             '''
 
-            working_sheet = []
+            working_sheet = [] #holds the row tuples
+            Drow = None #so that named tuple can be used after its set
         
-            for row in csv_reader:
-                working_sheet.append(row)
+            for row_num, row in enumerate(csv_reader):
+                if header_row is not 0: #args default to 0
+                    if row_num == header_row - 1: #enumerate starts at 0
 
-            workbook = []
-            sheet = {}
-            sheet['name'] = 'sheet1'
-            sheet['data'] = working_sheet
+                        header = []
+
+                        '''
+                        Sometimes csv's end up with left over columns.
+                        Instead of deleteing them here, if there are
+                        columns in the header row with no string name
+                        they are given a generic name 'col' + index in the list
+
+                        TODO
+                        This will need to be expanded to deal with headings
+                        that start with numbers or contain problematic chars
+                        like $ or %.
+                        '''
+
+                        for p_header_num, p_header_cell in enumerate(row):
+                            if p_header_cell == '':
+                                header.append('col' + str(p_header_num))
+                            else:
+                                header.append(p_header_cell)
+                        
+                        #create the named tuple object
+                        Drow = collections.namedtuple('Row',header)
+                    else:
+                        '''
+                        not sure why the ._make() works, but it does
+                        found example here http://www.daniweb.com/software-development/python/code/286925/exploring-named-tuples-python
+                        '''
+                        named_row = Drow._make(row)
+                        working_sheet.append(named_row)
+                else:
+                    '''
+                    with no header expected a basic tuple of the row
+                    is appended to the list of rows
+                    ''' 
+                    working_sheet.append(tuple(row))
+
+            '''
+            the workbook and worksheet construction is used here,
+            even though csv's won't have more than one worksheet, to maintain
+            consistency in functions that will access the sheet data later
+            '''
+            workbook = [] #for non-csv, a list of worksheets, here it's only 1
+            
+            sheet = Worksheet(sheet_name = file_name, 
+                                sheet_data = working_sheet)
+            
             workbook.append(sheet)
             
             self.sheets = workbook
@@ -46,22 +116,24 @@ class SheetReader(object):
 
             for xl_sheet_name in xl_sheet_names:
 
-                sheet = {}
-                sheet['name'] = xl_sheet_name
-                sheet['data'] = []
+                working_sheet = []
                 
                 xl_sheet = xl_reader.sheet_by_name(xl_sheet_name)
 
                 for xl_row in range(xl_sheet.nrows):
 
-                    sheet['data'].append(xl_sheet.row_values(xl_row))
+                    working_sheet.append(xl_sheet.row_values(xl_row))
+
+                sheet = Worksheet(sheet_name = xl_sheet_name, 
+                                    sheet_data = working_sheet)
 
                 workbook.append(sheet)
 
             self.sheets = workbook
         
         elif file_ext == '.xlsx':
-            xlx_reader = openpyxl.load_workbook(filename = file_location, use_iterators=True)
+            xlx_reader = openpyxl.load_workbook(filename = file_location, 
+                                                use_iterators=True)
 
             xlx_sheet_names = xlx_reader.get_sheet_names()
 
@@ -69,10 +141,8 @@ class SheetReader(object):
 
             for xlx_sheet_name in xlx_sheet_names:
 
-                sheet = {}
-                sheet['name'] = xlx_sheet_name
-                sheet['data'] = []
-                
+                working_sheet = []
+
                 xlx_sheet = xlx_reader.get_sheet_by_name(xlx_sheet_name)
 
                 for xlx_row in xlx_sheet.iter_rows():
@@ -82,19 +152,28 @@ class SheetReader(object):
                     for cell in xlx_row:
                         row_values.append(cell.internal_value)
 
-                    sheet['data'].append(row_values)
+                    working_sheet.append(row_values)
+
+                sheet = Worksheet(sheet_name = xlx_sheet_name, 
+                                    sheet_data = working_sheet)
 
                 workbook.append(sheet)
 
             self.sheets = workbook
         
         else:
-            print "unsupported file type .csv, .xls and .xlsx"
+            print "unsupported file type. Only .csv, .xls and .xlsx"
 
 
-    def row(self, sheet, row_choice):
-        self.sheets
+    def book_names(self):
+        for sheet in self.sheets:
+            yield sheet.sheet_name
 
+    def sheet_rows(self, sheet_name):
+        for sheet in self.sheets:
+            if sheet.sheet_name == sheet_name:
+                for row in sheet.sheet_data:
+                    yield row
 
     def col(self, sheet, col_choice):
         pass
